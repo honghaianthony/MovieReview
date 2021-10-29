84da2914781ccfa038f7f120e1f5ddcbffb51456
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-10-29 23:19:29 +0700

    change to const

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const db = require('./models/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 // Configures and sync models to the database
 db.sequelize.sync();
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
