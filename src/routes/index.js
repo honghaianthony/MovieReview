73137d00f66731d6de73d6f74fb285780e79c0d1
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-10-29 23:11:21 +0700

    login demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 
 router.use('/user', userRouter);
 
