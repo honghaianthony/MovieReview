b09061ee82b1782fe1dcdad1602cae59955f4785
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-11-04 10:37:49 +0700

    demo user

--- src/models/Users.js
+++ src/models/Users.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, Sequelize) => {
         },
         {
             sequelize,
-            modelName: 'user',
+            modelName: 'Users',
             timestamps: true,
             underscored: true,
         },
