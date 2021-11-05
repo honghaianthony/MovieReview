bb443cf774447c2018e47d946659146483935bbb
Anthony Hai <namsaoquan@gmail.com>
2021-11-05 09:23:58 +0700

    adding actor,rules,comments,collaborator and their scss

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userRouter = require('./users');
+const userRouter = require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get("/login", function (req, res, next) {
+    res.render("login");
 });
 
-
-
-
-
-router.use('/user', userRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
