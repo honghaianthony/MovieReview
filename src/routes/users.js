71e3f2342ad2e40477b95f039b4bec96e3137192
Anthony Hai <namsaoquan@gmail.com>
2021-11-09 22:15:08 +0700

    Fix routes login layout

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/UserController');
+const userController = require("../controllers/UserController");
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.render('login');
+
+router.get("/register", function (req, res, next) {
+    res.render("register");
 });
 
-router.get('/register', function(req, res, next) {
-    res.render('register');
-  });
-
-router.post('/register', userController.CreateUser);
+router.post("/register", userController.CreateUser);
 
 module.exports = router;
