6ce0c9ae30ce85b9668ee517a32b8433a0db1a7c
Anthony Hai <namsaoquan@gmail.com>
2021-11-24 11:18:12 +0700

    Admin Page for Review Film

--- src/models/User.js
+++ src/models/User.js
@@ -1,70 +1,70 @@
 module.exports = (sequelize, Sequelize) => {
-  class User extends Sequelize.Model {
-    static associate(models) {
-      User.hasMany(models.Review, { foreignKey: "userId" });
-      User.hasMany(models.Actor, { foreignKey: "userId" });
-      User.belongsToMany(models.Review, {
-        through: "Comment",
-        foreignKey: "userId",
-      });
-      User.belongsToMany(models.Review, {
-        through: "Rate",
-        foreignKey: "userId",
-      });
-      User.belongsToMany(models.Actor, {
-        through: "CommentActor",
-        foreignKey: "userId",
-      });
+    class User extends Sequelize.Model {
+        static associate(models) {
+            User.hasMany(models.Review, { foreignKey: "userId" });
+            User.hasMany(models.Actor, { foreignKey: "userId" });
+            User.belongsToMany(models.Review, {
+                through: "Comment",
+                foreignKey: "userId",
+            });
+            User.belongsToMany(models.Review, {
+                through: "Rate",
+                foreignKey: "userId",
+            });
+            User.belongsToMany(models.Actor, {
+                through: "CommentActor",
+                foreignKey: "userId",
+            });
+        }
     }
-  }
 
-  User.init(
-    {
-      username: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-        unique: {
-          args: true,
-          msg: "User with this username already exist.",
+    User.init(
+        {
+            username: {
+                type: Sequelize.STRING(255),
+                allowNull: false,
+                unique: {
+                    args: true,
+                    msg: "User with this username already exist.",
+                },
+            },
+            password: {
+                type: Sequelize.STRING(255),
+                allowNull: false,
+            },
+            fullName: {
+                type: Sequelize.STRING(255),
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING(255),
+                allowNull: false,
+                unique: {
+                    args: true,
+                    msg: "User with this email already exist.",
+                },
+            },
+            phone: {
+                type: Sequelize.STRING(255),
+            },
+            role: {
+                type: Sequelize.INTEGER,
+            },
+            dateOfBirth: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            avatar: {
+                type: Sequelize.STRING(255),
+            },
         },
-      },
-      password: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      fullName: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-        unique: {
-          args: true,
-          msg: "User with this email already exist.",
-        },
-      },
-      phone: {
-        type: Sequelize.STRING(255),
-      },
-      role: {
-        type: Sequelize.INTEGER,
-      },
-      dateOfBirth: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      avatar: {
-        type: Sequelize.STRING(255),
-      },
-    },
-    {
-      sequelize,
-      modelName: "User",
-      timestamps: true,
-      paranoid: true,
-    }
-  );
+        {
+            sequelize,
+            modelName: "User",
+            timestamps: true,
+            paranoid: true,
+        }
+    );
 
-  return User;
+    return User;
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
-
 const userRouter = require("./users");
+const adminRouter = require("./admin");
 const authRouter = require("./auth");
-const filmRouter = require('./film');
+const filmRouter = require("./film");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+    res.render("index", { title: "Express" });
 });
 
 router.use(authRouter);
-router.use("/film-review", filmRouter); 
-
+router.use("/film-review", filmRouter);
+router.use('/admin',adminRouter)
 
 router.get("/review-detail-squid-game", function (req, res, next) {
-  res.render("review-detail");
+    res.render("review-detail");
 });
-
 
 router.get("/aboutus", function (req, res, next) {
     res.render("aboutus");
 });
 router.get("/personal-page", function (req, res, next) {
-    res.render("personal-page",{ layout: "other" });
+    res.render("personal-page", { layout: "other" });
 });
+
+router.get("/admin-review-famous-movie", function (req, res, next) {
+    res.render("review-famous-admin.hbs", { layout: "admin" });
+});
+
+router.get("/admin-review-another-movie", function (req, res, next) {
+    res.render("another-film-admin.hbs", { layout: "admin" });
+});
+
+
 
 module.exports = router;
