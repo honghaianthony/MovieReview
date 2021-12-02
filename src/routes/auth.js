7ba8cee7730068d849f1c716f39d5562f4e5d6f1
ElseVn <67472527+UIT-19521292@users.noreply.github.com>
2021-12-02 13:17:45 +0700

    fix query and add AdminController

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,31 +6,31 @@ const passportAuth = require("../config/passport");
 const userController = require("../controllers/UserController");
 
 router.get(
-    "/login",
-    passportAuth.checkNotAuthenticated,
-    function (req, res, next) {
-        res.render("login", { layout: "other" });
-    }
+  "/login",
+  passportAuth.checkNotAuthenticated,
+  function (req, res, next) {
+    res.render("login", { layout: "other" });
+  }
 );
 
 router.post(
-    "/login",
-    passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
-        failureFlash: true,
-    })
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
 );
 router.get("/register", function (req, res, next) {
-    res.render("register", { layout: "other" });
+  res.render("register", { layout: "other" });
 });
 router.post("/register", userController.CreateUser);
 
 router.get("/forgot-password", function (req, res, next) {
-    res.render("forgot-password", { layout: "other" });
+  res.render("forgot-password", { layout: "other" });
 });
 router.get("/change-password", function (req, res, next) {
-    res.render("change-password", { layout: "other" });
+  res.render("change-password", { layout: "other" });
 });
 
 module.exports = router;
