184378632e0c56b287f9213185e934a9a275c966
ElseVn <67472527+UIT-19521292@users.noreply.github.com>
2021-12-03 23:10:16 +0700

    update adminController

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,4 +1,5 @@
 const e = require("cors");
+const { admin } = require("googleapis/build/src/apis/admin");
 const models = require("../models");
 
 module.exports = {
@@ -18,32 +19,37 @@ module.exports = {
         content: context,
         title,
       });
-
-      res.redirect("famous-actor");
+      res.render("famous-actor-admin", { layout: "admin", isDone: true });
     } catch (error) {
-      res.redirect("");
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
     }
   },
   CreateMovie: async function (req, res, next) {
     const { name, poster, rating, releaseYear, type, description } = req.body;
     try {
-      let types = type.split(" ,");
-      types.forEach(async (element) => {
-        await models.Genre.create({
-          type: element,
-        });
-      });
-      await models.Movie.create({
+      const getMovie = await models.Movie.create({
         name,
         poster,
         description,
         rating,
         releaseYear,
       });
-      //genremovie
-      res.redirect("");
+      console.log(getMovie.id);
+      let types = type.split(", ");
+      types.forEach(async (element) => {
+        const getGenre = await models.Genre.create({
+          type: element,
+        });
+        await models.GenreMovie.create({
+          movieId: getMovie.id,
+          genreId: getGenre.id,
+        });
+      });
+      res.redirect("review-famous-movie");
     } catch (error) {
-      res.redirect("");
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
     }
   },
   getInforActor: async function (req, res, next) {
@@ -63,11 +69,13 @@ module.exports = {
     });
   },
   deleteSelectedActor: async function (req, res, next) {
+    const { id } = req.body;
     try {
-      console.log(req.params);
+      await models.Actor.destroy({ where: { id } });
       res.redirect("famous-actor-list");
     } catch (error) {
-      res.redirect("famous-actor-list");
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
     }
   },
   getCTVForm: async function (req, res, next) {
@@ -80,9 +88,89 @@ module.exports = {
     }
   },
   deleteCTVRequest: async function (req, res, next) {
-    const {id} = req.params;
+    const { id } = req.params;
     try {
-      await models.CTV.destroy({where: {id}});
+      await models.CTV.destroy({ where: { id } });
+    } catch (error) {
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
+    }
+  },
+  getReviewList: async function (req, res, next) {
+    try {
+      const reviewList = await models.Review.findAll({
+        include: [{ model: models.Movie }],
+        raw: true,
+      });
+      let result = [];
+      reviewList.forEach((e) => {
+        result.push({
+          name: e["Movie.name"],
+          id: e.id,
+          content: e.content,
+          rate: e.rate,
+        });
+      });
+      res.render("another-film-admin", { layout: "admin", review: result });
+    } catch (error) {
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
+    }
+  },
+  deleteSelectedReview: async function (req, res, next) {
+    const { id } = req.body;
+    try {
+      await models.Review.destroy({ where: { id } });
+      res.redirect("review-another-movie");
+    } catch (error) {
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
+    }
+  },
+
+  getMovieList: async function (req, res, next) {
+    try {
+      let result = [];
+      const movie = await models.Movie.findAll();
+      movie.forEach(async (item) => {
+        let genres = [];
+        let sql = `select genres.type 
+                      from genremovies, genres
+                      where genremovies.genreId = genres.id
+                          and genremovies.movieId = :id`;
+        const genre = await models.sequelize.query(sql, {
+          replacements: {
+            id: item.id,
+          },
+          type: models.Sequelize.QueryTypes.SELECT,
+        });
+        genre.forEach((e) => {
+          genres.push(e.type);
+        });
+        result.push({
+          id: item.id,
+          name: item.name,
+          nation: item.nation,
+          description: item.description,
+          rating: item.rating,
+          poster: item.poster,
+          releaseYear: item.releaseYear,
+          genre: genres.join(", ").toString(),
+        });
+      });
+      res.render("review-movie-admin", { layout: "admin", movie: result });
+    } catch (error) {
+      res.status(500);
+      res.render("error", { message: "Something went wrong!", layout: false });
+    }
+  },
+  deleteSelectedMovie: async function (req, res, next) {
+    const { id } = req.body;
+    try {
+      await models.GenreMovie.destroy({ where: { movieId: id } });
+      await models.Review.destroy({ where: { movieId: id } });
+      await models.Movie.destroy({ where: { id } });
+      res.redirect("review-movie");
     } catch (error) {
       res.status(500);
       res.render("error", { message: "Something went wrong!", layout: false });
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,12 +6,11 @@ const passportAuth = require("../config/passport");
 
 router.use(passportAuth.checkAdmin);
 
-router.get("/review-movie", function (req, res, next) {
-  res.render("review-movie-admin", { layout: "admin" });
-});
+router.get("/review-movie", adminController.getMovieList);
+router.post("/review-movie", adminController.deleteSelectedMovie);
 
 router.get("/famous-actor-list", adminController.getInforActor);
-router.post("/famous-actor-list/:id", adminController.deleteSelectedActor);
+router.post("/famous-actor-list", adminController.deleteSelectedActor);
 
 router.get("/famous-actor", function (req, res, next) {
   res.render("famous-actor-admin", { layout: "admin" });
@@ -21,10 +20,11 @@ router.post("/famous-actor", adminController.CreateActorPost);
 router.get("/review-famous-movie", function (req, res, next) {
   res.render("review-famous-admin", { layout: "admin" });
 });
+router.post("/review-famous-movie", adminController.CreateMovie);
 
-router.get("/review-another-movie", function (req, res, next) {
-  res.render("another-film-admin", { layout: "admin" });
-});
+router.get("/review-another-movie", adminController.getReviewList);
+router.post("/review-another-movie", adminController.deleteSelectedReview);
+
 router.get("/user-management", function (req, res, next) {
   res.render("user-mannagement-admin", { layout: "admin" });
 });
